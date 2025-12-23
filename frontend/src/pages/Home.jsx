b2ba9f6bebdf17b22dca2133b0175aef8d861a52
additi123 <mehtaadditi@gmail.com>
2025-12-23 12:07:30 -0500

    completed hero page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-import Hero from "../components/Hero";
+
+import Hero from '../components/Hero/Hero.jsx';
+import NavBar from "../components/NavBar/NavBar.jsx";
 export default function HomePage() {
   return (
     <div>
+      <NavBar />
       <Hero />
 
     </div>
